2a45590c799ac4082206b2ce1223239f21274c7c
OCDkirby <mangacreeperyt@gmail.com>
2022-04-21 14:40:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 pushd Lexicalli && ./build.sh && popd && cp Syntan/bin/syntan bin/syntan && \
-pushd Syntan && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli # && \ 
+pushd Syntan && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli && \ 
+mkdir -p bin && g++ src/*.cpp -Llib -Iinclude -lKirbParse_Static -o bin/jazzy
