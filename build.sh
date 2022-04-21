52799bdbd62606ba30caa7391564c8bb8956a010
bfahrenfort <mangacreeperyt@gmail.com>
2022-04-21 16:01:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-mkdir -p bin &&
+mkdir -p bin && mkdir -p lib && 
 pushd Lexicalli && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli && 
 pushd Syntan && ./build.sh && popd && cp Syntan/bin/syntan bin/syntan && 
 pushd KirbParse && cmake . && make && popd && cp KirbParse/src/kirbparse.h include/kirbparse.h && cp KirbParse/lib/libKirbParse_Static.a lib/libKirbParse_Static.a
