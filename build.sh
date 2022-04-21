1f7aa2c2c9deb64e70e2328de9bfb9bc55e9ded2
bfahrenfort <mangacreeperyt@gmail.com>
2022-04-21 15:47:33 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-pushd Lexicalli && ./build.sh && popd && cp Syntan/bin/syntan bin/syntan && 
-pushd Syntan && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli && 
-mkdir -p bin && g++ src/*.cpp -Llib -Iinclude -lKirbParse_Static -o bin/jazzy -Wno-write-strings
+mkdir -p bin &&
+pushd Lexicalli && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli && 
+pushd Syntan && ./build.sh && popd && cp Syntan/bin/syntan bin/syntan && 
+g++ src/*.cpp -Llib -Iinclude -lKirbParse_Static -o bin/jazzy -Wno-write-strings
