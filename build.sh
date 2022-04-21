6f12f6acf33f54f11b309ffcf617efe8a49f6de4
bfahrenfort <mangacreeperyt@gmail.com>
2022-04-21 15:56:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir -p bin &&
 pushd Lexicalli && ./build.sh && popd && cp Lexicalli/bin/calli bin/calli && 
 pushd Syntan && ./build.sh && popd && cp Syntan/bin/syntan bin/syntan && 
+pushd KirbParse && cmake . && make && popd && cp KirbParse/src/kirbparse.h include/ && cp KirbParse/lib/libKirbParse_Static.a lib/
 g++ src/*.cpp -Llib -Iinclude -lKirbParse_Static -o bin/jazzy -Wno-write-strings
